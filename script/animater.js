d1aaadbad0dc56830d0c9bb4c291a3199160d18e
Maritedo <62166314+Maritedo@users.noreply.github.com>
2023-12-19 21:35:36 +0800

    initial commit & create readme.md

---
+++ script/animater.js
@@ -0,0 +1,168 @@
+function animater(element) {
+    return new Animater(element)
+}
+
+class Animater {
+    /** @type {{duration: number, timing: (value: number) => number, circle: boolean, value: number }} */
+    static defaultOptions = {
+        duration: 600,
+        timing: Animater.easeOutQuint,
+        circle: false,
+        value: 0
+    };
+    /** @type {{from: number, to: number, value: number}} */
+    static defaultMeta = {
+        from: 0,
+        to: 1,
+        value: 0
+    };
+    /** @type {Element} */
+    element;
+    /** @type {{ states: { tick: { last: number, start: number }, pad: number, flag: boolean, start: number, target: number, current: number, newValue: number, completed: boolean }, values: Map<string, { from: number, to: number, value: number }> , timing: (e: number) => number, duration: number, circle: boolean }[]} */
+    propertyGroups = [];
+    /** @type {(()=>any)[]} */
+    payloads = [];
+    /** @type {boolean} */
+    lock = false;
+
+    constructor(element) {
+        this.element = element;
+    }
+
+    onTick() {
+        let completed = true;
+        for (const prop of this.propertyGroups) {
+            const states = prop.states;
+            if (states.flag) {
+                states.flag = false;
+                states.pad = (states.target = states.newValue) - (states.start = states.current);
+                states.tick.start = states.tick.last;
+            }
+            const tick = Date.now();
+            states.tick.last = tick;
+            if (states.completed) continue;
+            else if (states.current == states.target) {
+                states.start = states.target;
+                states.completed = true;
+                continue;
+            }
+
+            completed = false;
+            const offset = (states.target - states.start) / (states.target - states.current);
+            const offsetTime = keepInside(0, (tick - states.tick.start) / prop.duration, 1);
+            if (offset < 0.001) states.current = states.target;
+            else {
+                if (prop.circle) {
+                    const route1 = states.target - states.start;
+                    const delta = Math.abs(route1);
+                    const route2 = route1 > 0 ? (delta - 1) : (1 - delta);
+                    const route = delta < 0.5 ? route1 : route2;
+                    states.current = (1 + states.start + route * prop.timing(offsetTime)) % 1;
+                } else {
+                    states.current = states.start + states.pad * prop.timing(offsetTime);
+                }
+            }
+            for (const key of prop.values.keys()) {
+                const values = prop.values.get(key);
+                const { from, to } = values;
+                values.value = from + (to - from) * states.current;
+            }
+        }
+        if (completed) this.lock = false;
+        else {
+            this.payloads.forEach(e => e(this.propertyGroups));
+            requestAnimationFrame(() => this.onTick());
+        }
+    }
+
+    proxy(payload) {
+        this.payloads.push(payload);
+        return this;
+    }
+
+    unregistryProxy(callback) {
+        const re = [];
+        this.payloads.forEach(e => {
+            if (e !== callback) re.push(e);
+        });
+        this.payloads = re;
+    }
+
+    /**
+     * @param {{ states: { tick: { last: number, start: number }, pad: number, flag: boolean, start: number, target: number, current: number, newValue: number, completed: boolean }, values: Map<string, { from: number, to: number, value: number }> , timing: (e: number) => number, duration: number, circle: boolean }} propGroup 
+     * @param {number} defaultVal
+     */
+    gen(propGroup, defaultVal = 0) {
+        let then;
+        const go = (value) => {
+            if (propGroup.states.target != value) {
+                propGroup.states.completed = false;
+                if (!this.lock) {
+                    this.lock = true;
+                    propGroup.states.tick.last = propGroup.states.tick.start = Date.now();
+                    propGroup.states.pad = (propGroup.states.target = value) - propGroup.states.start;
+                    this.onTick();
+                } else {
+                    propGroup.states.newValue = value;
+                    propGroup.states.flag = true;
+                }
+            }
+            return then;
+        }
+        const set = (value) => {
+            propGroup.states.completed = true;
+            propGroup.states.current = propGroup.states.start = propGroup.states.current = value;
+            propGroup.states.pad = 0;
+        }
+        const reset = () => go(defaultVal);
+        const get = (keyName) => propGroup.values.get(keyName).value;
+        return (then = { go, set, reset, get });
+    }
+
+    property(prop, options) {
+        const props = new Map();
+        const _animater_ = this;
+        const group = (options) => {
+            const { timing, duration, circle, value } = Object.assign({}, Animater.defaultOptions, options);
+            const propGroup = {
+                states: {
+                    tick: {
+                        last: 0,
+                        start: 0
+                    },
+                    pad: 0,
+                    flag: false,
+                    start: value,
+                    target: value,
+                    current: value,
+                    newValue: 0,
+                    completed: true
+                },
+                values: props,
+                timing,
+                circle,
+                duration
+            };
+            _animater_.propertyGroups.push(propGroup);
+            return _animater_.gen(propGroup, value);
+        }
+        const property = (prop, options) => {
+            const { from, to, value } = Object.assign({}, Animater.defaultMeta, options);
+            props.set(prop, { from, to, value });
+            return { property, group };
+        }
+        return property(prop, options);
+    }
+
+    static easeOutQuint(x) {
+        return 1 - Math.pow(1 - x, 5);
+    }
+
+    static easeInOutCubic(x) {
+        return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+    }
+}
+
+function keepInside(min, val, max) {
+    return Math.max(Math.min(val, max), min)
+}
---
+++ script/lib.js
@@ -0,0 +1,212 @@
+const CACHE = new Map();
+function $(selector, cache = false, env = document) {
+    const ElementType = {
+        DOCUMENT: 0,
+        WINDOW: 1,
+        ELEMENT: 2
+    };
+
+    let element, TYPE;
+    const forEvery = (cbfn) => element.forEach(ele => cbfn(ele));
+
+    if (selector === document) {
+        TYPE = ElementType.DOCUMENT;
+        element = [selector];
+    }
+    else if (selector === window) {
+        TYPE = ElementType.WINDOW;
+        element = [selector];
+    }
+    else if (typeof selector === "string") {
+        TYPE = ElementType.ELEMENT;
+        if (!cache) {
+            element = env.querySelectorAll(selector);
+        }
+        else if (!(element = CACHE.get({ selector, env }))) {
+            element = env.querySelectorAll(selector);
+            CACHE.set({ selector, env }, element);
+        }
+        element = Array.from(element);
+    }
+    else if (selector instanceof Element) {
+        TYPE = ElementType.ELEMENT;
+        element = [selector];
+    }
+    else if (selector instanceof Array) {
+        TYPE = ElementType.ELEMENT;
+        element = selector;
+    }
+    else if (selector === null) {
+        return null;
+    }
+    else {
+        console.log(selector);
+        throw new Error(selector);
+    }
+
+    function log() {
+        forEvery(e => console.log(e));
+        return then;
+    }
+
+    function remove() {
+        if (TYPE === ElementType.ELEMENT)
+            element.forEach(ele => {
+                ele.parentElement && ele.parentElement.removeChild(ele);
+            })
+        return then;
+    }
+
+    function on(evtType, evtCallback) {
+        evtType.split(/\s+/).forEach(evt => {
+            forEvery(e => e.addEventListener(evt, evtCallback));
+        });
+        return then;
+    }
+
+    function off(evtType, evtCallback) {
+        evtType.split(/\s+/).forEach(evt => {
+            forEvery(e => e.removeEventListener(evt, evtCallback));
+        });
+        return then;
+    }
+
+    function addClass(...classes) {
+        forEvery(e => {
+            e.classList.add(...classes)
+        });
+        return then;
+    }
+
+    function removeClass(...classes) {
+        forEvery(e => {
+            e.classList.remove(...classes)
+        });
+        return then;
+    }
+
+    function toggleClass(...classes) {
+        forEvery(e => {
+            e.classList.toggle(...classes)
+        });
+        return then;
+    }
+
+    function hasClass(...classes) {
+        return !classes.find(className => {
+            return Array.from(element).find(ele => {
+                return !ele.classList.contains(className)
+            })
+        })
+    }
+
+    function prop(propName, value) {
+        forEvery(ele => {
+            ele[propName] = value
+        })
+        return then;
+    }
+
+    function attr(attrName, value) {
+        forEvery(ele => {
+            ele.setAttribute(attrName, value);
+        })
+        return then;
+    }
+
+    function css(attrName, value) {
+        forEvery(ele => {
+            ele.style.setProperty(attrName, value);
+        });
+        return then;
+    }
+
+    function style(attrName, value) {
+        forEvery(ele => {
+            ele.style[attrName] = value;
+        });
+        return then;
+    }
+
+    function append(...ls) {
+        element.push(...ls);
+    }
+
+    function query(selector, cache = false) {
+        let res;
+        forEvery(e => {
+            if (res) res.append(...$(selector, cache, e).eles);
+            else res = $(selector, cache, e);
+        })
+        return res;
+    }
+
+    function filter(selector) {
+        let res = [];
+        forEvery(e => {
+            if (e.matches(selector))
+                res.push(e)
+        });
+        return $(res);
+    }
+
+    const then = {
+        log,
+        remove,
+        on,
+        off,
+        toggleClass,
+        addClass,
+        removeClass,
+        hasClass,
+        each: (fn) => (forEvery(e => fn($(e))), then),
+        append,
+        query,
+        filter,
+        prop,
+        attr,
+        css,
+        style,
+        eles: element,
+        get ele() { return $(element[0]) },
+        get pure() { return element[0] },
+        get parent() { return $(element[0].parentElement) },
+        get next() { return $(element[0].nextElementSibling) },
+        get prev() { return $(element[0].previousElementSibling) },
+        get length() { return element.length }
+    };
+    return then;
+    /**
+     * 假定使用者不会傻到在$(document)或$(window)上尝试使用addClass之类的方法
+     */
+}
+
+function refSize(element, callback = null) {
+    const boundingBox = element.getBoundingClientRect();
+    const res = {
+        width: boundingBox.width,
+        height: boundingBox.height,
+        top: boundingBox.top,
+        right: boundingBox.right,
+        left: boundingBox.left,
+        bottom: boundingBox.bottom,
+        x: boundingBox.x,
+        y: boundingBox.y
+    };
+    const resizeObserver = new ResizeObserver(entries => {
+        console.log(entries);
+        ({
+            width: res.width,
+            height: res.height,
+            top: res.top,
+            right: res.right,
+            left: res.left,
+            bottom: res.bottom,
+            x: res.x,
+            y: res.y
+        } = entries[0].contentRect);
+        callback && callback();
+    });
+    resizeObserver.observe(element);
+    return { box: res, stop: resizeObserver.disconnect };
+}
